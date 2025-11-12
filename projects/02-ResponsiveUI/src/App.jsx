2b56db34602229a797f1335583ef12111203fda1
Trilochanprasad B Hilli <tptrilochanprasad@gmail.com>
2025-11-12 23:08:08 +0530

    responsive tailwind

---
+++ projects/02-ResponsiveUI/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Layout } from "./components/Layout.jsx";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Layout />
+    </div>
+  );
+}
--- projects/02-undoredocounter/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center text-4xl font-bold">
-      02-undoredocounter ✅ Tailwind v4 working perfectly!
-    </div>
-  );
-}
